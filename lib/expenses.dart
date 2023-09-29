5bc973c8085f29198d2617e7bf2081a0c7a0f493
Julio Gonzalez <antonio15198able@gmail.com>
2023-09-29 17:30:55 -0500

    Create expenses list widget

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:expense_tracker/expenses_list.dart';
+import 'models/expense.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -11,10 +13,28 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: 'Refrescos',
+        amount: 49.50,
+        date: DateTime.now(),
+        category: Category.food),
+    Expense(
+        title: 'Gasolina',
+        amount: 200,
+        date: DateTime.now(),
+        category: Category.travel)
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: const [Text("Gráfica"), Text("Gastos")]),
+      body: Column(children: [
+        const Text("Gráfica"),
+        Expanded(
+          child: ExpensesList(expenses: _registeredExpenses),
+        ),
+      ]),
     );
   }
 }
