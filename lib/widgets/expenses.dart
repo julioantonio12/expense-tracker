f2a523e147f17bc1a72413dccb9680d9c0b53cd6
Julio Gonzalez <antonio15198able@gmail.com>
2023-10-02 16:48:49 -0600

    Implement remove with dissmisible

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -41,6 +41,12 @@ class _ExpensesState extends State<Expenses> {
     });
   }
 
+  void _removeExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.remove(expense);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -56,7 +62,10 @@ class _ExpensesState extends State<Expenses> {
       body: Column(children: [
         const Text("Gráfica"),
         Expanded(
-          child: ExpensesList(expenses: _registeredExpenses),
+          child: ExpensesList(
+            expenses: _registeredExpenses,
+            onRemoveExpense: _removeExpense,
+          ),
         ),
       ]),
     );
